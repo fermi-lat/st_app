46db7e65841cfe3019ddce62f511f9d9174eceda
peachey <>
2004-04-15 15:47:37 +0000

    Migrate to StApp.

--- src/test/test_main.cxx
+++ src/test/test_main.cxx
@@ -4,21 +4,20 @@
 
 #include <iostream>
 
-#include "st_app/IApp.h"
+#include "st_app/StApp.h"
+#include "st_app/StAppFactory.h"
 
 /** \class TestApp1
     \brief Application singleton for test_st_app.
 */
-class TestApp1 : public st_app::IApp {
+class TestApp1 : public st_app::StApp {
   public:
-    /** \brief Create AppExample1 object, identifying it to the base class.
-    */
-    TestApp1(): st_app::IApp("test_st_app") {}
-
     /** \brief Perform the action needed by this application. This will be called by the standard main.
     */
     virtual void run() {
       std::cout << "Hello world" << std::endl;
     }
-} the_application; // Create the application object.
+};
 
+// Factory which can create an instance of the class above.
+st_app::StAppFactory<TestApp1> g_factory;
