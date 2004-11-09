96805002fa46cfc03e11a261f7636e273909ce7c
peachey <>
2004-11-09 20:01:08 +0000

    Call application factory's configureApp method, to set up streams using universal parameters chatter and debug. Determine whether to catch exceptions at runtime by calling the factory's getDebugMode method.

--- src/ballistic_main.cxx
+++ src/ballistic_main.cxx
@@ -19,6 +19,8 @@ int main(int argc, char ** argv) {
   int status = 0;
   st_app::StApp * this_st_app = 0;
 
+  bool debug = false;
+
   try {
 #ifdef TRAP_FPE
     // Add floating point exception traps.
@@ -29,18 +31,24 @@ int main(int argc, char ** argv) {
     // cannot/should not start.
     st_app::StApp::processCommandLine(argc, argv);
 
-#ifndef ST_APP_DEBUG
-    // Not in debug mode, so create and run the application here, inside the top-level try block.
-    try {
-      // Try using the singleton StAppFactory to create the application:
-      this_st_app = st_app::IStAppFactory::instance().createApp();
-    } catch(const std::logic_error & x) {
-      throw std::logic_error("Failed to get an StAppFactory singleton: client must define one");
-    }
+    // Read standard parameters to set up chatter, debug mode, etc.
+    st_app::IStAppFactory::instance().configureApp();
 
-    // Run the application.
-    if (0 != this_st_app) this_st_app->run();
-#endif
+    // Determine whether debugging was selected, either at runtime or compile time.
+    debug = st_app::IStAppFactory::instance().getDebugMode();
+
+    if (!debug) {
+      // Not in debug mode, so create and run the application here, inside the top-level try block.
+      try {
+        // Try using the singleton StAppFactory to create the application:
+        this_st_app = st_app::IStAppFactory::instance().createApp();
+      } catch(const std::logic_error & x) {
+        throw std::logic_error("Failed to get an StAppFactory singleton: client must define one");
+      }
+
+      // Run the application.
+      if (0 != this_st_app) this_st_app->run();
+    }
 
   } catch (const std::exception & x) {
     // Return a non-zero exit code:
@@ -56,15 +64,15 @@ int main(int argc, char ** argv) {
     std::cerr << "Caught " << type_name << " at the top level: " << x.what() << std::endl;
   }
 
-#ifdef ST_APP_DEBUG
-  // In debug mode, so create and run the application here, outside of any try-catch,
-  // so that the debugger is more useful for finding exceptions at their source.
-  // Use the singleton StAppFactory to create the application:
-  this_st_app = st_app::IStAppFactory::instance().createApp();
+  if (debug) {
+    // In debug mode, so create and run the application here, outside of any try-catch,
+    // so that the debugger is more useful for finding exceptions at their source.
+    // Use the singleton StAppFactory to create the application:
+    this_st_app = st_app::IStAppFactory::instance().createApp();
 
-  // Run the application.
-  if (0 != this_st_app) this_st_app->run();
-#endif
+    // Run the application.
+    if (0 != this_st_app) this_st_app->run();
+  }
 
   // Clean up:
   delete this_st_app;
