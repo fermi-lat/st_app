ef0ed6ea9776a45090ac7f153a64c5a67a445262
peachey <>
2004-12-30 15:58:27 +0000

    Add switch which enables/disables prompting, used e.g. to suppress command line prompts in GUI mode.

--- src/AppParGroup.cxx
+++ src/AppParGroup.cxx
@@ -10,10 +10,10 @@ namespace st_app {
 
   // Construct parameter group, getting arguments from the application and calling hoops base class.
   AppParGroup::AppParGroup(const std::string & comp_name):
-    hoops::ParPromptGroup(StApp::getArgc() - 1, StApp::getArgv() + 1, comp_name) {}
+    hoops::ParPromptGroup(StApp::getArgc() - 1, StApp::getArgv() + 1, comp_name), m_prompt_mode(true) {}
 
   // Copy construct parameter group.
-  AppParGroup::AppParGroup(const AppParGroup & group): hoops::ParPromptGroup(group) {}
+  AppParGroup::AppParGroup(const AppParGroup & group): hoops::ParPromptGroup(group), m_prompt_mode(group.m_prompt_mode) {}
 
   // Assignments, using hoops to do the real work.
   hoops::IParGroup & AppParGroup::operator =(const AppParGroup & group)
@@ -21,5 +21,15 @@ namespace st_app {
    
   hoops::IParGroup & AppParGroup::operator =(const hoops::IParGroup & group)
     { hoops::ParPromptGroup::operator =(group); return *this; }
+
+  void AppParGroup::Prompt() {
+    if (m_prompt_mode) hoops::ParPromptGroup::Prompt();
+  }
+
+  void AppParGroup::Prompt(const std::string & par_name) {
+    if (m_prompt_mode) hoops::ParPromptGroup::Prompt(par_name);
+  }
+
+  void AppParGroup::suppressPrompts(bool suppress) { m_prompt_mode = !suppress; }
 
 }
