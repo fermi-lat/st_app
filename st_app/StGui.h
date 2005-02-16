e79f6ac7eb028538f9b2711ebd0fb9c8156a010d
peachey <>
2005-02-16 19:00:54 +0000

    Add tab folder for switched parameters. Improve layout and look.

--- st_app/StGui.h
+++ st_app/StGui.h
@@ -2,15 +2,18 @@
 #define st_app_StGui_h
 
 #include <list>
+#include <map>
 
 #include "hoops/hoops.h"
 #include "st_graph/Engine.h"
 #include "st_graph/IEventReceiver.h"
+#include "st_graph/ITabFolder.h"
 #include "st_graph/Placer.h"
 #include "st_stream/StreamFormatter.h"
 
 namespace st_app {
 
+  class AppParGroup;
   class StApp;
 
   class ParWidget : public st_graph::IEventReceiver {
@@ -31,14 +34,12 @@ namespace st_app {
 
       st_graph::IFrame * getLabel();
 
-      void alignLabel(const st_graph::HPlacer & h);
-
       const std::string & getName() const;
 
       const std::string & getValue() const;
 
     protected:
-      ParWidget(st_graph::Engine & engine, hoops::IPar * par);
+      long entryWidth(hoops::IPar * par) const;
 
     private:
       st_graph::Engine & m_engine;
@@ -48,11 +49,15 @@ namespace st_app {
       st_graph::IFrame * m_value;
       st_graph::IFrame * m_open;
       hoops::IPar * m_par;
+      bool m_stretch;
   };
 
   class StEventReceiver : public st_graph::IEventReceiver {
     public:
-      StEventReceiver(StApp * m_app);
+      typedef std::map<std::string, ParWidget *> ParWidgetCont;
+      typedef std::map<std::string, st_graph::ITabFolder *> TabFolderCont;
+
+      StEventReceiver(st_graph::Engine & engine, AppParGroup & par_group, StApp * m_app);
 
       virtual ~StEventReceiver();
 
@@ -66,20 +71,28 @@ namespace st_app {
 
       virtual void createMainFrame();
 
-      virtual ParWidget * createParWidget(hoops::IPar * par);
+      virtual ParWidget * createParWidget(hoops::IPar * par, st_graph::IFrame * parent);
 
     private:
+      bool parseRange(const hoops::IPar * par, std::list<std::string> & range);
+      void getParent(const hoops::IPar * par, std::list<st_graph::IFrame *> & parent);
+
       st_stream::StreamFormatter m_os;
       st_graph::Engine & m_engine;
-      std::list<ParWidget *> m_par_widget;
+      AppParGroup & m_par_group;
+      ParWidgetCont m_par_widget;
+      TabFolderCont m_tab_folder;
+      std::map<st_graph::IFrame *, st_graph::IFrame *> m_parent;
       st_graph::IFrame * m_main;
       st_graph::IFrame * m_group_frame;
       st_graph::IFrame * m_run;
       st_graph::IFrame * m_cancel;
       StApp * m_app;
       ParWidget * m_widest;
+      long m_tab_height;
   };
 
+  typedef StEventReceiver StAppGui;
 }
 
 #endif
