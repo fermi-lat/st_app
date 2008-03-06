e6ffe96914f1889bd605226e2e4b741ca7a2542e
glast <>
2008-03-06 18:00:55 +0000

    Added f2c as a dependency

--- st_appLib.py
+++ st_appLib.py
@@ -1,4 +1,4 @@
-#$Id$
+#$Id: st_appLib.py,v 1.3 2008/02/26 05:45:34 glastrm Exp $
 def generate(env, **kw):
 	if not kw.get('depsOnly',0):
 		env.Tool('addLibrary', library = ['st_app'])
@@ -6,6 +6,7 @@ def generate(env, **kw):
 	env.Tool('st_graphLib')
 	env.Tool('st_streamLib')
 	env.Tool('facilitiesLib')
+	env.Tool('addLibrary', library = env['f2cLibs'])
 
 def exists(env):
 	return 1
